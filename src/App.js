3eaab4f1dd7d66afdc9ef4798f75b2ef9bcc9db7
httpsCarlos <141081762+httpsCarlos@users.noreply.github.com>
2024-05-27 21:10:22 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useMemo } from 'react';
 
 function App(){
   const[tarefas, setTarefas] = useState([]);
 
   const [input, setInput] = useState("");
+
+  const[numero, setNumero] = useState(0);
+
+  const calculo = useMemo(()=>{
+    let resultado = 0;
+    for(let i = 0; i< numero * 1000; i++){
+      resultado+= i;
+    }
+    return resultado;
+  }, [numero]);
 
   useEffect(() => {
     const tarefasStorage = localStorage.getItem('tarefas');
@@ -33,7 +43,12 @@ function App(){
 
       <input type='text' value={input} onChange={e => setInput(e.target.value)}/>
       <button type='button' onClick={adicionarTarefa}>Adicionar</button>
+
+      <p>Número: {numero}</p>
+      <p>Resultado do calculo: {calculo}</p>
+      <button onClick={() => setNumero(numero+1)}>Incrementar</button>
     </div>
+    
   );
 
 
